25a61b138da6ece89300371edef901f0677a14e0
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-06-22 11:25:41 -0400

    Added Pojo class example

---
+++ src/main/java/com/example/SpringPractice/PojoPractice/Main.java
@@ -0,0 +1,16 @@
+package com.example.SpringPractice.PojoPractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setMake("McLaren");
+        car.setModel("570S Spider");
+        car.setYear("2022");
+
+        System.out.println("Make: " + car.getMake());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+
+    }
+}
